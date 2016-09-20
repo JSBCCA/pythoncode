7dc746788f8d3565726c1a495773dfbc012acf79
James Sibert <jsibert@basecampcodingacademy.org>
2016-09-20 11:07:26 -0500

    added tests

--- space_separated.py
+++ space_separated.py
@@ -1,8 +1,30 @@
-txt = input("Enter your space separated entries:\n").strip().lower()
-sym = "1234567890)(*&^%$#@!-=_+][}{\\|;:\'\"/?>.<,"
-print(sorted(set((''.join(c for c in txt if c not in sym)).split()) - set("")))
+def space_sep(txt):
+    sym = "1234567890)(*&^%$#@!-=_+][}{\\|;:\'\"/?>.<,"
+    return (sorted(set((''.join(c for c in txt
+                                if c not in sym)).split()) - set("")))
+
+# txt = input("Enter your space separated entries:\n").strip().lower()
+# sym = "1234567890)(*&^%$#@!-=_+][}{\\|;:\'\"/?>.<,"
+# print(sorted(set((''.join(c for c in txt if c not in sym)).split()) - set("")))
 
 # print(sorted(set((''.join(
 #     c for c in (input('Enter your space separated entries:\n').strip().lower())
 #     if c not in '1234567890)(*&^%$#@!-=_+][}{\\|;:\'\"/?>.<,')).split()) - set(
 #         '')))
+
+
+def test_space_sep():
+    assert space_sep("a b c") == ['a', 'b', 'c']
+    assert space_sep("123245") == []
+    assert space_sep("abc, and") == ['abc', 'and']
+
+
+def test_space_sep_two():
+    assert space_sep("!!!aaaaaaa") == ['aaaaaaa']
+    assert space_sep("a1b2, 5678 and ; 67 for") == ['ab', 'and', 'for']
+    assert space_sep("once upon a time,") == ['a', 'once', 'time', 'upon']
+
+
+if __name__ == '__main__':
+    txt = input("Enter your space separated entries:\n").strip().lower()
+    print(space_sep(txt))
--- word_analysis.py
+++ word_analysis.py
@@ -1,12 +1,47 @@
-text = input("What text would you like to analyze? ")
-letters = ''.join(c for c in text if c.isalpha())
-vowels = ''.join(c for c in text if c.lower() in "aeiou")
-print("-".join(letters), "\nTotal Text Length:", str(len(text)),
-      "\nNumber of Letters:", str(len(letters)), "\nNumber of Vowels:",
-      str(len(vowels)))
+def word_analyze(text):
+    letters = ''.join(c for c in text if c.isalpha())
+    vowels = ''.join(c for c in text if c.lower() in "aeiou")
+    return ("-".join(letters)) + "\nTotal Text Length: " + str(len(
+        text)) + "\nNumber of Letters: " + str(len(
+            letters)) + "\nNumber of Vowels: " + str(len(vowels))
+
+# text = input("What text would you like to analyze? ")
+# letters = ''.join(c for c in text if c.isalpha())
+# vowels = ''.join(c for c in text if c.lower() in "aeiou")
+# print("-".join(letters), "\nTotal Text Length:", str(len(text)),
+#       "\nNumber of Letters:", str(len(letters)), "\nNumber of Vowels:",
+#       str(len(vowels)))
 
 # t = input('What text would you like to analyze? ')
 # l = (''.join(c for c in t if c.isalpha()))
 # print('-'.join(l), '\nTotal Text Length:', str(len(t)), '\nNumber of Letters:',
 #       str(len(l)), '\nNumber of Vowels:',
 #       str(len(''.join(c for c in t if c.lower() in 'aeiou'))))
+
+
+def test_word_analyze():
+    assert word_analyze("abcdefg") == """a-b-c-d-e-f-g
+Total Text Length: 7
+Number of Letters: 7
+Number of Vowels: 2"""
+    assert word_analyze("12345") == """
+Total Text Length: 5
+Number of Letters: 0
+Number of Vowels: 0"""
+
+
+def test_word_analyze_two():
+    assert word_analyze("James Sibert") == """J-a-m-e-s-S-i-b-e-r-t
+Total Text Length: 12
+Number of Letters: 11
+Number of Vowels: 4"""
+    assert word_analyze(
+        "Base234 Camp888 coding7;") == """B-a-s-e-C-a-m-p-c-o-d-i-n-g
+Total Text Length: 24
+Number of Letters: 14
+Number of Vowels: 5"""
+
+
+if __name__ == '__main__':
+    text = input("What text would you like to analyze? ")
+    print(word_analyze(text))
