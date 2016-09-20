f0ab242c52d4ed8aed8f1bc3c34b4cd44018ca80
James Sibert <jsibert@basecampcodingacademy.org>
2016-09-20 10:15:57 -0500

    made shorter versions

--- space_separated.py
+++ space_separated.py
@@ -1,3 +1,8 @@
 txt = input("Enter your space separated entries:\n").strip().lower()
 sym = "1234567890)(*&^%$#@!-=_+][}{\\|;:\'\"/?>.<,"
 print(sorted(set((''.join(c for c in txt if c not in sym)).split()) - set("")))
+
+# print(sorted(set((''.join(
+#     c for c in (input('Enter your space separated entries:\n').strip().lower())
+#     if c not in '1234567890)(*&^%$#@!-=_+][}{\\|;:\'\"/?>.<,')).split()) - set(
+#         '')))
--- word_analysis.py
+++ word_analysis.py
@@ -4,3 +4,9 @@ vowels = ''.join(c for c in text if c.lower() in "aeiou")
 print("-".join(letters), "\nTotal Text Length:", str(len(text)),
       "\nNumber of Letters:", str(len(letters)), "\nNumber of Vowels:",
       str(len(vowels)))
+
+# t = input('What text would you like to analyze? ')
+# l = (''.join(c for c in t if c.isalpha()))
+# print('-'.join(l), '\nTotal Text Length:', str(len(t)), '\nNumber of Letters:',
+#       str(len(l)), '\nNumber of Vowels:',
+#       str(len(''.join(c for c in t if c.lower() in 'aeiou'))))
