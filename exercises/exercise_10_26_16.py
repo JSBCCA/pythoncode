852aa061c1edf54a7d35b824f820f673fdc4fb13
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-26 10:48:43 -0500

    added note

--- exercises/exercise_10_26_16.py
+++ exercises/exercise_10_26_16.py
@@ -1,4 +1,4 @@
-def palin(p):
+def palin(p):  # str.isalpha is function form of the method .isalpha()
     np = list(filter(str.isalpha, p.lower()))  # remove non-letters
     return np == np[::-1] and len(np) > 0  # check if palindrome
 
