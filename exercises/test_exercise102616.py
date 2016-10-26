b674408974de707d1cdf9be955477ce4372d0234
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-26 11:06:35 -0500

    shorter

--- exercises/test_exercise102616.py
+++ exercises/test_exercise102616.py
@@ -3,30 +3,8 @@ from exercise_10_26_16 import *
 
 
 def test_palin():
-    text = 'mom'
-    pt = palin(text)
-    assert pt is True
-
-
-def test_multi_palin():
-    text = 'wasitaratisaw'
-    pt = palin(text)
-    assert pt is True
-
-
-def test_false():
-    text = 'dog'
-    pt = palin(text)
-    assert pt is False
-
-
-def test_symbols():
-    text = '9874^&'
-    pt = palin(text)
-    assert pt is False
-
-
-def test_palin_with_symbols():
-    text = "Go hang a salami I'm a lasagna hog."
-    pt = palin(text)
-    assert pt is True
+    assert palin('mom')
+    assert palin('wasitaratisaw')
+    assert palin('dog') is False
+    assert palin('9874^&') is False
+    assert palin("Go hang a salami I'm a lasagna hog.")
