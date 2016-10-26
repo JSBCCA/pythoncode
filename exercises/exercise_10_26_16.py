154b54dd25a34468126c42506b32eb582457fed3
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-26 11:08:28 -0500

    combined

--- exercises/exercise_10_26_16.py
+++ exercises/exercise_10_26_16.py
@@ -3,6 +3,15 @@ def palin(p):  # str.isalpha is function form of the method .isalpha()
     return np == np[::-1] and len(np) > 0  # check if palindrome
 
 
+# py.test test_exercise102616.py --cov=exercise_10_26_16.py --cov-report=html
+def test_palin():
+    assert palin('mom')
+    assert palin('wasitaratisaw')
+    assert palin('dog') is False
+    assert palin('9874^&') is False
+    assert palin("Go hang a salami I'm a lasagna hog.")
+
+
 if __name__ == '__main__':
     t = input("Text: ")
     print(palin(t))
