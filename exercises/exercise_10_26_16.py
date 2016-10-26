aadc71fdf20f16066cff6c2a6e099408ecb68786
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-26 10:26:30 -0500

    shorter

--- exercises/exercise_10_26_16.py
+++ exercises/exercise_10_26_16.py
@@ -1,8 +1,8 @@
-def palindrome(p):  # remove non-letters, make lower
-    np = ''.join(list(filter(lambda c: c.isalpha(), list(p.strip().lower()))))
-    return (np == np[::-1]) and (len(np) > 0)  # check if palindrome
+def palin(p):
+    np = list(filter(str.isalpha, p.lower()))  # remove non-letters
+    return np == np[::-1] and len(np) > 0  # check if palindrome
 
 
 if __name__ == '__main__':
-    t = input("Give text: ")
-    print(palindrome(t))
+    t = input("Text: ")
+    print(palin(t))
--- exercises/test_exercise102616.py
+++ exercises/test_exercise102616.py
@@ -2,31 +2,31 @@
 from exercise_10_26_16 import *
 
 
-def test_palindrome():
+def test_palin():
     text = 'mom'
-    pt = palindrome(text)
+    pt = palin(text)
     assert pt is True
 
 
-def test_multi_palindrome():
+def test_multi_palin():
     text = 'wasitaratisaw'
-    pt = palindrome(text)
+    pt = palin(text)
     assert pt is True
 
 
 def test_false():
     text = 'dog'
-    pt = palindrome(text)
+    pt = palin(text)
     assert pt is False
 
 
 def test_symbols():
     text = '9874^&'
-    pt = palindrome(text)
+    pt = palin(text)
     assert pt is False
 
 
 def test_palin_with_symbols():
     text = "Go hang a salami I'm a lasagna hog."
-    pt = palindrome(text)
+    pt = palin(text)
     assert pt is True
