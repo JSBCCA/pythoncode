cd86015281121d6958d77a541b23e0edd7e54475
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-26 10:19:50 -0500

    shorter

--- exercises/exercise_10_26_16.py
+++ exercises/exercise_10_26_16.py
@@ -1,11 +1,8 @@
-def palindrome(t):  # remove non-letters, make lower
-    nt = ''.join(list(filter(lambda c: c.isalpha(), list(t.strip().lower()))))
-    if (str(nt) == str(nt)[::-1]) and (len(nt) > 0):  # check for palindrome
-        return True
-    else:
-        return False
+def palindrome(p):  # remove non-letters, make lower
+    np = ''.join(list(filter(lambda c: c.isalpha(), list(p.strip().lower()))))
+    return (np == np[::-1]) and (len(np) > 0)  # check if palindrome
 
 
 if __name__ == '__main__':
-    yours = input("Insert your palindrome: ")
-    print(palindrome(yours))
+    t = input("Give text: ")
+    print(palindrome(t))
