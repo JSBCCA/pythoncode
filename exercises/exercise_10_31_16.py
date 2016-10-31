96752bc79370b3f3c37b90dc4c4262bb1d8d0f68
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-31 09:20:05 -0500

    today's exercise

---
+++ exercises/exercise_10_31_16.py
@@ -0,0 +1,31 @@
+with open('phone_data.txt', 'r') as file:
+    phone_numbers = file.read().strip().split('\n')
+
+
+def change_format(phonelist):
+    # change phone numbers to (000) 000-0000 format and add them to new_phone_data.txt
+    newlist = []
+    with open('new_phone_data.txt', 'w') as file:
+        for p in phonelist:
+            if len(p) == 10:
+                phone = '(' + str(p[:3]) + ') ' + str(p[3:6]) + '-' + str(p[
+                    6:]) + '\n'
+                file.write(phone)
+            elif len(p) == 11:
+                phone = '(' + str(p[1:4]) + ') ' + str(p[4:7]) + '-' + str(p[
+                    7:]) + '\n'
+                file.write(phone)
+            elif len(p) == 12:
+                phone = '(' + str(p[:3]) + ') ' + str(p[4:7]) + '-' + str(p[
+                    8:]) + '\n'
+                file.write(phone)
+            elif len(p) == 14:
+                phone = '(' + str(p[2:5]) + ') ' + str(p[6:9]) + '-' + str(p[
+                    10:]) + '\n'
+                file.write(phone)
+            else:
+                file.write("Unknown format\n")
+
+
+if __name__ == '__main__':
+    change_format(phone_numbers)
