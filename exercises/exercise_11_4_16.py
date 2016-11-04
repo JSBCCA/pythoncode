355d650795c85732aa8690aaca398a0c0efe46ee
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-04 09:33:30 -0500

    today's exercise

---
+++ exercises/exercise_11_4_16.py
@@ -0,0 +1,24 @@
+def words_to_count(words):
+    """str -> str"""
+    result = ""
+    words = words.strip().split()
+    for word in range(len(words)):
+        if words[word] == words[-1]:
+            result += str(len(words[word]))
+        else:
+            result += str(len(words[word])) + " "
+    return result
+
+
+# py.test exercise_11_4_16.py --cov=exercise_11_4_16.py --cov-report=html
+def test_words_to_count():
+    assert words_to_count("I am a goofy goober!") == "1 2 1 5 7"
+    assert words_to_count("") == ""
+
+
+if __name__ == "__main__":
+    with open("bacon.txt", 'r') as bacon:
+        bacon_words = bacon.read()
+    counted_words = words_to_count(bacon_words)
+    with open("bacon_count.txt", 'w') as bacon_count:
+        bacon_count.write(counted_words)
