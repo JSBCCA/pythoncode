f80417624f2929d38c6b640da1e676f73d6c3d3c
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-10 10:08:48 -0600

    shortened

--- exercises/exercise_11_10_16.py
+++ exercises/exercise_11_10_16.py
@@ -1,8 +1,9 @@
 import random
 ranks, suits = list('A123456789TJQK'), list('SDCH')
-deck = []
-for suit in suits:
-    for num in ranks:
-        deck.append(num + suit)
+# deck = []
+# for suit in suits:
+#     for num in ranks:
+#         deck.append(num + suit)
+deck = [s + r for r in ranks for s in suits]
 random.shuffle(deck)
 print(deck)
