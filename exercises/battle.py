86dcc6e222ee247e670cbb9ebdd8f9a308ceefc6
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-10 14:12:43 -0600

    personal project

--- exercises/battle.py
+++ exercises/battle.py
@@ -1,18 +1,28 @@
 chars = {
-    'the human': [10, 'good'],
+    'frisk': [10, 'good'],
+    'dummy': [0, '???'],
+    'mad dummy': [80, '???'],
+    'glad dummy': [5, 'good'],
     'toriel': [80, 'good'],
+    'napstablook': [10, 'good'],
     'sans': [1, 'good'],
     'papyrus': [20, 'good'],
     'alphys': [8, 'good'],
     'mettaton': [30, 'good'],
     'mettaton ex': [47, 'good'],
-    'muffet': [38.8, 'good']
+    'mettaton neo': [90, 'good'],
+    'muffet': [38.8, '???']
     'flowey': [3, 'evil'],
+    'photoshop flowey': [9999, 'evil'],
     'asgore': [80, 'good'],
     'undyne': [50, 'good'],
+    'undyne the undying': [99, 'good'],
     'asriel dreemurr': [9999, 'snevil'],
-    'chara': [99, 'evil']
+    'asriel': [20, 'good'],
+    'chara': [99, 'evil'],
+    'gaster': [2, '???']
 }
+# 197
 
 first = input("First battler: ").lower().strip()
 second = input("Second battler: ").lower().strip()
