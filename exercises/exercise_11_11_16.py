74d1d03fee84b0cefb55d2e1aa78b0bcb71c87f9
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-11 09:37:04 -0600

    today's exercise

--- exercises/exercise_11_11_16.py
+++ exercises/exercise_11_11_16.py
@@ -1,10 +1,10 @@
 import random
-str_rand_num = "99"  # start with 99
+randnum = "99"  # start with 99
 while True:  # loop
-    new_str_rand_num = str(random.randint(2, 150))  # set new randint
-    print(str_rand_num + " little bugs in the code\n" + str_rand_num +
-          " little bugs\nTake one down, patch it around\n" + new_str_rand_num +
-          " little bugs in the code")
-    str_rand_num = new_str_rand_num  # set new randint to be used again
+    new_randnum = str(random.randint(2, 150))  # set new randint
+    print("""{0} little bugs in the code\n{0} little bugs
+Take one down, patch it around\n{1} little bugs in the code"""
+          .format(randnum, new_randnum))
+    randnum = new_randnum  # set new randint to be used again
     if input().lower().strip() == "q":  # ask if they want to quit
         break  # if so, quit
