04200c87190eef1db1b58a3c762b6addd2c3087f
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-11 10:18:51 -0600

    today's exercise

--- exercises/exercise_11_11_16.py
+++ exercises/exercise_11_11_16.py
@@ -3,6 +3,6 @@ r_num = "99"  # start with 99
 while True:  # loop
     print("{0} little bugs in the code\n{0} little bugs".format(r_num))
     r_num = str(random.randint(2, 150))  # set new randint
-    if input("""Take one down, patch it around
-{} little bugs in the code\n""".format(r_num)).lower().strip() == "q":
+    if input("""Take one down, patch it around\n{} little bugs in the code
+""".format(r_num)).lower().strip() == "q":
         break  # option to quit
