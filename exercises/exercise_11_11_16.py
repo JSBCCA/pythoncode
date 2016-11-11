774dcf4991bfbf03bfc45a5aa9befafde1ea7e43
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-11 09:28:01 -0600

    today's exercise

--- exercises/exercise_11_11_16.py
+++ exercises/exercise_11_11_16.py
@@ -1,5 +1,4 @@
 import random  # import random
-import sys  # import ability to exit
 str_rand_num = "99"  # start with 99
 while True:  # start loop
     new_str_rand_num = str(random.randint(2, 150))  # set new randint
@@ -9,4 +8,4 @@ while True:  # start loop
     str_rand_num = new_str_rand_num  # set new randint to be used again
     quit = input().lower().strip()  # ask if they want to quit
     if quit == "q":  # if so,
-        sys.exit()  # quit
+        break  # quit
