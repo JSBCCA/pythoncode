048f6c6b9504f411c9697f3402839841711b380d
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-11 09:10:06 -0600

    today's exercise

--- exercises/exercise_11_11_16.py
+++ exercises/exercise_11_11_16.py
@@ -1,18 +1,12 @@
-from random import randint
-import sys
-# start with 99
-str_rand_num = "99"
-# loop
-while True:
-    # set new randint
-    new_str_rand_num = str(randint(2, 150))
-    # use both randints
-    print(str_rand_num + """ little bugs in the code
-""" + str_rand_num + " little bugs\nTake one down, patch it around\n" +
-          new_str_rand_num + " little bugs in the code")
-    # change new randint to be used again
-    str_rand_num = new_str_rand_num
-    # ask if they want to quit
-    quit = input().lower().strip()
-    if quit == "q":
-        sys.exit()
+from random import randint  # import randint
+import sys  # import ability to exit
+str_rand_num = "99"  # start with 99
+while True:  # start loop
+    new_str_rand_num = str(randint(2, 150))  # set new randint
+    print(str_rand_num + " little bugs in the code\n" + str_rand_num +
+          " little bugs\nTake one down, patch it around\n" + new_str_rand_num +
+          " little bugs in the code")  # use both randints
+    str_rand_num = new_str_rand_num  # set new randint to be used again
+    quit = input().lower().strip()  # ask if they want to quit
+    if quit == "q":  # if so,
+        sys.exit()  # quit
