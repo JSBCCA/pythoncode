2e43ef74ef5d4f661b09eda58e08add12febd464
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-16 10:56:21 -0600

    shorter

--- exercises/exercise_11_16_16.py
+++ exercises/exercise_11_16_16.py
@@ -1,19 +1,13 @@
-# put info into list of lists
 list_of_list = []
-with open("Sacramentorealestatetransactions.csv", 'r') as sret:
-    for line in sret:
-        sret_list = line.split(",")
-        list_of_list.append(sret_list)
-# get list of sell prices
+with open("Sacramentorealestatetransactions.csv", 'r') as file:
+    for line in file:
+        list_of_list.append(line.split(","))
+# make list of sell prices
 sales_list = []
 for sale in list_of_list[1:]:
     sales_list.append(int(sale[9]))
-# find average
-av = 0
-for sell in sales_list:
-    av += sell
-av = av / len(sales_list)
 # print
 print("Highest: $" + str(max(sales_list)))
 print("Lowest: $" + str(min(sales_list)))
-print("Average: $" + str(round(av, 2)))
+av = round(sum(sales_list) / len(sales_list), 2)
+print("Average: $" + str(av))
