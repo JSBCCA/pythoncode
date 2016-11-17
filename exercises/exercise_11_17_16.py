9a44b9ee6d1e139f2a1330a59b126cbf0a327103
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-17 09:38:20 -0600

    today's exercise

--- exercises/exercise_11_17_16.py
+++ exercises/exercise_11_17_16.py
@@ -1,15 +1,11 @@
-list_of_list = []
 with open("Sacramentorealestatetransactions.csv", 'r') as file:
-    for line in file:
-        list_of_list.append(line.split(","))
-# take input
-highest_price = int(input())
-lowest_price = int(input())
-# find info based on input
-new_list = []
-for item in list_of_list[1:]:
-    if (int(item[9]) >= lowest_price) and (int(item[9]) <= highest_price):
-        new_list.append(item)
-# show homes
-for h in new_list:
+    sret_list = file.read().splitlines()  # open file
+highest_price = int(input("Highest price: "))  # take input
+lowest_price = int(input("Lowest price: "))  # take info
+new_list = []  # create list to append info to
+for item in sret_list[1:]:  # find info based on input
+    info = item.split(",")  # split lines by commas
+    if (int(info[9]) >= lowest_price) and (int(info[9]) <= highest_price):
+        new_list.append(info)  # add info to list created earlier
+for h in new_list:  # show homes
     print("\n${}\n{}, {}, {}, {}".format(h[9], h[0], h[1], h[2], h[3]))
