096a7a453deedf8409987aa44cc4ab8989230b88
James Sibert <jsibert@basecampcodingacademy.org>
2016-12-02 10:23:36 -0600

    bored project

--- exercises/number_add.py
+++ exercises/number_add.py
@@ -1,6 +1,17 @@
+# don't go above 20,000,000
+
+# version for level up
 x = int(input())
-y = x
+y = x / 2
 while x > 1:
     x -= 1
     y += x
 print(y)
+
+# version for adding
+# x = int(input())
+# y = x
+# while x > 1:
+#     x -= 1
+#     y += x
+# print(y)
