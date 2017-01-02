338aa7f66cf3f44bcc2a9cd2a359dc84211b2271
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-02 09:04:16 -0600

    exercise

--- exercises/exercise_12_6_16.py
+++ exercises/exercise_12_6_16.py
@@ -1,25 +1,23 @@
 while True:
-    nouns = input("Give 5 nouns separated by commas. ").split(", ")
-    if len(nouns) == 5:
+    v = input('Give 8 verbs separated by commas. ').split(', ')
+    if len(v) == 8:
         break
 while True:
-    adjectives = input("Give 5 adjectives separated by commas. ").split(", ")
-    if len(adjectives) == 5:
+    n = input('Give 6 nouns separated by commas. ').split(', ')
+    if len(n) == 6:
         break
 while True:
-    verbs = input("Give 6 verbs separated by commas. ").split(", ")
-    if len(verbs) == 6:
+    a = input('Give 5 adjectives separated by commas. ').split(', ')
+    if len(a) == 5:
         break
 
-print("\nRudolph the " + adjectives[0].title() + "-Nosed " + nouns[0].title() +
-      "\nHad a very " + adjectives[1] + " nose\nAnd if you ever " + verbs[0] +
-      "ed it\nYou would even say it glows\nAll of the other " + nouns[0] +
-      "\nUsed to " + verbs[1] + " and call him " + nouns[1] +
-      "\nThey never let " + adjectives[2] + " Rudolph\n" + verbs[2] +
-      " in any " + nouns[0] + " games\nThen one " + adjectives[3] +
-      " Christmas Eve\nSanta " + verbs[3] + ' to say\n"Rudolph, with your ' +
-      nouns[2] + " so " + adjectives[4] + "\nWon't you " + verbs[4] + " my " +
-      nouns[3] + ' tonight?"\nThen how the reindeer ' + verbs[5] +
-      'ed him\nAs they shouted out with glee\n"Rudolph the ' + adjectives[
-          0].title() + "-Nosed " + nouns[0] + "\nYou'll go down in " + nouns[4]
-      + '!"')
+print('\nRudolph the ' + a[0].title() + '-Nosed ' + n[0].title() +
+      '\nHad a very ' + a[1] + ' nose\nAnd if you ever ' + v[0] +
+      'ed it\nYou would even say it ' + v[1] + 's\nAll of the other ' + n[0] +
+      '\nUsed to ' + v[2] + ' and call him ' + n[1] + '\nThey never let ' + a[
+          2] + ' Rudolph\n' + v[3] + ' in any ' + n[0] + ' games\nThen one ' +
+      a[3] + ' Christmas Eve\nSanta ' + v[4] + ' to say\n"Rudolph, with your '
+      + n[2] + ' so ' + a[4] + "\nWon't you " + v[5] + ' my ' + n[3] +
+      ' tonight?"\nThen how the reindeer ' + v[6] + 'ed him\nAs they ' + v[7] +
+      'ed out with ' + n[4] + '\n"Rudolph the ' + a[0].title() + '-Nosed ' + n[
+          0] + "\nYou'll go down in " + n[5] + '!"')
