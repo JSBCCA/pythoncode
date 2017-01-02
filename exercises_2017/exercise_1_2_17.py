916faddc50bbb2f83c6335f82cb936c47bf94123
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-02 09:21:56 -0600

    changed name

---
+++ exercises_2017/exercise_1_2_17.py
@@ -0,0 +1,15 @@
+# Number of words
+# Number of paragraphs
+# 20 most common words
+
+from collections import Counter
+
+with open("alice_in.txt", 'r') as file:
+    alice = file.read().split()  # list of all of our words
+
+l = []
+for word in alice:
+    l.append("".join(filter(str.isalpha, word)))
+
+# Counter makes an ordered dictionary
+print(Counter(l).most_common(20))
