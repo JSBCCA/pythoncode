52285ce39276dc5ae867918bfbc29b8fef0744e2
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-02 10:00:01 -0600

    twenty most common words counts an empty space as a word still. also need to figure out how to catch paragraphs...

--- 2017_exercises/exercise_1_2_17.py
+++ 2017_exercises/exercise_1_2_17.py
@@ -1,20 +1,26 @@
-# Number of words
 # Number of paragraphs
-# 20 most common words
 
 from collections import Counter
 
 with open("alice_in.txt", 'r') as file:
-    alice = file.read().split()  # list of all of our words
+    alice = file.readlines()  # list of all of our words
+
+alice = alice[32:3371]
+alice = "".join(alice)
+alice = alice.split(" ")
 
 l = []
 for word in alice:
     l.append("".join(filter(str.isalpha, word)))
 
-# Counter makes an ordered dictionary
+    # lambda x: .isalpha and len(x) > 0, word
+
 print("Number of words: " + str(len(l)))
+
 print("Number of paragraphs: ")
+
 print("Twenty most common words: ")
+# Counter makes an ordered dictionary
 placeholder_variable = Counter(l).most_common(20)
 for i in range(len(placeholder_variable)):
     if placeholder_variable[i] == placeholder_variable[-1]:
