9ec4b167b810992ac27c7b3e78a6d667d63b7f91
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-02 10:08:22 -0600

    cleaned up code a little

--- 2017_exercises/exercise_1_2_17.py
+++ 2017_exercises/exercise_1_2_17.py
@@ -1,5 +1,3 @@
-# Number of paragraphs
-
 from collections import Counter
 
 with open("alice_in.txt", 'r') as file:
@@ -14,17 +12,17 @@ for word in alice:
     if len(word) > 0:
         l.append("".join(filter(str.isalpha, word)))
 
-    # lambda x: .isalpha and len(x) > 0, word
-
 print("Number of words: " + str(len(l)))
 
 print("Number of paragraphs: ?")
 
 print("Twenty most common words: ")
+
 # Counter makes an ordered dictionary
-placeholder_variable = Counter(l).most_common(20)
-for i in range(len(placeholder_variable)):
-    if placeholder_variable[i] == placeholder_variable[-1]:
-        print(str(placeholder_variable[i][0]))
+twenty = Counter(l).most_common(20)
+
+for i in range(len(twenty)):
+    if twenty[i] == twenty[-1]:
+        print(str(twenty[i][0]))
     else:
-        print(str(placeholder_variable[i][0]) + ", ", end="")
+        print(str(twenty[i][0]) + ", ", end="")
