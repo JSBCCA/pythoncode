20140772ee10b9af98a2f76f1759c8cfcf726e43
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-03 09:45:55 -0600

    shortened code

--- 2017_exercises/exercise_1_3_17.py
+++ 2017_exercises/exercise_1_3_17.py
@@ -1,13 +1,9 @@
 def trans(x):
     t = str.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
                       "mnopqrstuvwxyzabcdefghijklMNOPQRSTUVWXYZABCDEFGHIJKL")
-    return str(x).translate(t)
+    return x.translate(t)
 
 
-with open("alice_in.txt", 'r') as file:
-    alice = file.read()
-
-ciph = trans(alice)
-
-with open("alice_cipher.txt", 'w') as file:
-    file.write(ciph)
+with open("alice_in.txt", 'r') as alice:
+    with open("alice_cipher.txt", 'w') as ciph:
+        ciph.write(trans(alice.read()))
