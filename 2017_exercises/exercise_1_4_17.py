c9fe702800ca55a0bd310f1a611581918342be2b
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-04 09:28:47 -0600

    figured out how to reverse the entire text, but having trouble flipping individual words

--- 2017_exercises/exercise_1_4_17.py
+++ 2017_exercises/exercise_1_4_17.py
@@ -1,3 +1,19 @@
-# Your task is to write two new files:
-# The entire text is reversed ("base camp coding" becomes "gnidoc pmac esab")
-# Each word is reversed ("base camp coding" becomes "esab pmac gnidoc")
+def text_reverse(x):  # The entire text is reversed
+    return x[::-1]
+
+
+def word_reverse(x):  # each word is reversed
+    # somehow call text_reverse on each word, but keep spaces and newlines
+    return rev
+
+
+with open("alice_in.txt", 'r') as file:
+    alice = file.read()
+
+tr = text_reverse(alice)
+with open("reversed_alice.txt", 'w') as file:
+    file.write(tr)
+
+wr = word_reverse(alice)
+with open("alice_words_reversed.txt", 'w') as file:
+    file.write(wr)
