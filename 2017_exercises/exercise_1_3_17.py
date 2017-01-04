c1e48ab18f584718cc23bb082c9b41a0df517108
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-04 08:46:34 -0600

    todays exercise

--- 2017_exercises/exercise_1_3_17.py
+++ 2017_exercises/exercise_1_3_17.py
@@ -1,9 +1,13 @@
 def trans(x):
     t = str.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
                       "mnopqrstuvwxyzabcdefghijklMNOPQRSTUVWXYZABCDEFGHIJKL")
-    return x.translate(t)
+    return str(x).translate(t)
 
 
-with open("alice_in.txt", 'r') as alice:
-    with open("alice_cipher.txt", 'w') as ciph:
-        ciph.write(trans(alice.read()))
+with open("alice_in.txt", 'r') as file:
+    alice = file.read()
+
+ciph = trans(alice)
+
+with open("alice_cipher.txt", 'w') as file:
+    file.write(ciph)
---
+++ 2017_exercises/exercise_1_4_17.py
@@ -0,0 +1,3 @@
+# Your task is to write two new files:
+# The entire text is reversed ("base camp coding" becomes "gnidoc pmac esab")
+# Each word is reversed ("base camp coding" becomes "esab pmac gnidoc")
