8a8e23b25824a52f51b12532ffbbf77ff8b63eb5
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-04 09:43:18 -0600

    working on keeping newlines. otherwise finished

--- 2017_exercises/exercise_1_4_17.py
+++ 2017_exercises/exercise_1_4_17.py
@@ -1,19 +1,29 @@
-def text_reverse(x):  # The entire text is reversed
-    return x[::-1]
+with open("alice_in.txt", 'r') as alice:
+    x = alice.read()
 
-
-def word_reverse(x):  # each word is reversed
-    # somehow call text_reverse on each word, but keep spaces and newlines
-    return rev
-
-
-with open("alice_in.txt", 'r') as file:
-    alice = file.read()
-
-tr = text_reverse(alice)
 with open("reversed_alice.txt", 'w') as file:
-    file.write(tr)
+    file.write(x[::-1])
 
-wr = word_reverse(alice)
+rev = " ".join(w[::-1] for w in x.split())
 with open("alice_words_reversed.txt", 'w') as file:
-    file.write(wr)
+    file.write(rev)
+
+# def text_reverse(x):  # The entire text is reversed
+#     return x[::-1]
+#
+#
+# def word_reverse(x):  # each word is reversed
+#     rev = " ".join(w[::-1] for w in x.split())
+#     return rev
+#
+#
+# with open("alice_in.txt", 'r') as file:
+#     alice = file.read()
+#
+# tr = text_reverse(alice)
+# with open("reversed_alice.txt", 'w') as file:
+#     file.write(tr)
+#
+# wr = word_reverse(alice)
+# with open("alice_words_reversed.txt", 'w') as file:
+#     file.write(wr)
