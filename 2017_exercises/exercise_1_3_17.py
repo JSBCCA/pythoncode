eaac63d672a876a4ce07c32bfedc7591b34b2885
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-04 09:33:44 -0600

    added shortened version

--- 2017_exercises/exercise_1_3_17.py
+++ 2017_exercises/exercise_1_3_17.py
@@ -4,10 +4,14 @@ def trans(x):
     return str(x).translate(t)
 
 
-with open("alice_in.txt", 'r') as file:
-    alice = file.read()
+with open("alice_in.txt", 'r') as alice:
+    with open("alice_cipher.txt", 'w') as ciph:
+        ciph.write(trans(alice.read()))
 
-ciph = trans(alice)
-
-with open("alice_cipher.txt", 'w') as file:
-    file.write(ciph)
+# with open("alice_in.txt", 'r') as file:
+#     alice = file.read()
+#
+# ciph = trans(alice)
+#
+# with open("alice_cipher.txt", 'w') as file:
+#     file.write(ciph)
